4429b62129fa43abbb91bbc35b616c4a7b94d38f
ZC21GNL <72438913+GreenNeem@users.noreply.github.com>
2020-11-06 18:27:48 +0900

    template

---
+++ template/TemplateSyntax.vue
@@ -0,0 +1,12 @@
+<template>
+	<div>
+		<h2>this is template syntax</h2>
+		<li v-for="(lnk,id) in lstOflinks" v-bind:key="id">
+			<a v-bind:href="lnk.value">{{lnk.text}}</a>
+		</li>	
+		<input v-model="firstName" placeholder="First Name">
+		<input v-model="lastName" placeholder="Last Name">
+		<p>Message is: {{ firstName+' '+lastName }}</p>
+		<p>Testing computed property: {{ComputProperTest}}</p>
+	</div>
+</template>
