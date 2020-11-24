e5fe4671554efb4d1f7bcba7862cffbb1255a177
ZC21GNL <72438913+GreenNeem@users.noreply.github.com>
2020-11-24 21:55:13 +0900

    StudyEmit
    参照リンク先あり

---
+++ template/emit.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div v-for="(item, index) in items" :key="index">
+      <child @child-event="handler($event, index)">
+        {{ item.name }}
+      </child>
+    </div>
+  </div>
+</template>
+
+<script>
+import Child from './emit_child.vue'
+
+export default {
+  components: {
+    Child,
+  },
+  data:function() {
+    // body...
+    return {
+      items: [
+        { name: 'たろう' },
+        { name: 'はなこ' },
+      ],
+    }
+  },
+  methods: {
+    handler(event, index) {
+      //ボタンごとにメソットを追加しているから
+      //読み込まれたメソットから返した値が要素ごとに追加される。
+      //パラメータとして指定されたIndexでオブジェクトの索引が分かる。
+      console.log('first '+event);
+      console.log('second ' + index);
+    },
+  },
+}
+</script>
+<!-- 参照：https://qiita.com/tsuka-rinorino/items/fef323e9e83260b1ae6f -->
---
+++ template/emit_child.vue
@@ -0,0 +1,16 @@
+<template>
+  <button @click="event">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  methods: {
+    event() {
+      this.$emit('child-event', '山田');
+    },
+  },
+}
+</script>
+<!-- 参照：https://qiita.com/tsuka-rinorino/items/fef323e9e83260b1ae6f -->
