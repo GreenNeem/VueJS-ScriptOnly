77e9252cf82b99d6da6658ecb25041e623377a21
ZC21GNL <72438913+GreenNeem@users.noreply.github.com>
2020-11-30 16:29:11 +0900

    template

--- template/withoutWebpack.js
+++ template/withoutWebpack.js
@@ -1,10 +1,14 @@
+import UserList from './UserList.js';
+
 export default {
+  name: `App`,
+  components: {
+    UserList,
+  },
   template: `
     <div class="container mx-auto p-4">
       <h1>Users</h1>
+      <user-list class="mt-6"></user-list>
     </div>
   `,
-  //vueCli環境でのソース結合を試行したため。
-  //発生したエラーによりVueCliサイトのConfiguration Referenceを参照しながら設定変更必要。
-
 };
