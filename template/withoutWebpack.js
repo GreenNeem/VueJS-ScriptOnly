bccd50ec402a468b2c2c9eac8265511ef973adaa
ZC21GNL <72438913+GreenNeem@users.noreply.github.com>
2020-11-30 16:18:35 +0900

    template

---
+++ template/withoutWebpack.js
@@ -0,0 +1,10 @@
+export default {
+  template: `
+    <div class="container mx-auto p-4">
+      <h1>Users</h1>
+    </div>
+  `,
+  //vueCli環境でのソース結合を試行したため。
+  //発生したエラーによりVueCliサイトのConfiguration Referenceを参照しながら設定変更必要。
+
+};
---
+++ template/withoutWebpackCcombine_vue-edit.vue
@@ -0,0 +1,32 @@
+<template>
+	<div>
+		<childtemplate @enlarge-text="onEnlargeText($event)">
+		{{postFontSize}}
+		</childtemplate>
+		<testtemplate></testtemplate>
+	</div>
+	
+</template>
+<script>
+	import childtemplate from './vue-edit-child.vue'
+	import testtemplate from '@/components/withoutWebpack.js'
+	export default{
+		components:{
+			childtemplate,
+			testtemplate
+		},
+		data:function(){
+			return {
+				postFontSize:0,
+			}
+		},
+		methods:{
+			onEnlargeText:function(enlargeAmount) {
+				// this.postFontSize += enlargeAmount
+				// body...
+				console.log("here!")
+				console.log(enlargeAmount);
+			}
+		}
+	}
+</script>
